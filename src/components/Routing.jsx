2b553ed12e7bedecff8fbe79486534c05dbdcfed
Minh Nguyen <minhiq149@gmail.com>
2022-05-09 11:06:38 +0700

    Init project, create the search, news, images and videos pages (videos page still has errors)

---
+++ src/components/Routing.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Routes, Route, Navigate} from 'react-router-dom';
+
+import Results from "./Results";
+import Search from "./Search";
+
+const Routing = () => {
+    return (
+        <div className='p-4'>
+            <Routes>
+                <Route exact path='/' element={<Navigate to='/search'/>}/>
+                <Route exact path='/search' element={<Results/>}/>
+                <Route exact path='/image' element={<Results/>}/>
+                <Route exact path='/news' element={<Results/>}/>
+                <Route exact path='/video' element={<Results/>}/>
+            </Routes>
+        </div>
+    )
+}
+
+export default Routing;
---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Links from "./Links";
+
+const Search = () => {
+    return (
+        <div>
+            Search
+            <Links/>
+        </div>
+    )
+}
+
+export default Search;
